07e09f2455102c20105d92c8a067c3cc87868f97
Captdam <captdam@beardle.com>
2023-07-02 03:57:45 -0400

    Update object.php

--- template/object.php
+++ template/object.php
@@ -1,16 +1,14 @@
 <?php
-	header('Content-Type: '.$BW->site->getData('Aux'));
-	header('Cache-Control: private, max-age=3600');
-
-	if ($BW->site->getTemplate()[1] == 'blob') {
-		echo $BW->site->getData('Content');
-	} else if ($BW->site->getTemplate()[1] == 'local') {
-		$resource = Bearweb_Site_ResourceDir.$BW->site->getData('Content');
+	header('Content-Type: '.$BW->site->get(Bearweb_Site::FIELD_AUX));
+	if ($BW->site->get(Bearweb_Site::FIELD_TEMPLATE)[1] == 'blob' || $BW->site->get(Bearweb_Site::FIELD_TEMPLATE)[1] == 'externalimage') {
+		echo $BW->site->get(Bearweb_Site::FIELD_CONTENT);
+	} else if ($BW->site->get(Bearweb_Site::FIELD_TEMPLATE)[1] == 'local') {
+		$resource = Bearweb_Site_ResourceDir.$BW->site->get(Bearweb_Site::FIELD_CONTENT);
 		if (!file_exists($resource)) throw new BW_WebServerError('Resource not found: '.$resource, 500);
 		echo file_get_contents($resource);
 	} else {
-		$template = Bearweb_Site_TemplateDir.'object_'.$BW->site->getTemplate()[1].'.php';
-		if (!file_exists($template)) throw new BW_WebServerError('Secondary object template not found: '.$BW->site->getTemplate()[1], 500);
+		$template = Bearweb_Site_TemplateDir.'object_'.$BW->site->get(Bearweb_Site::FIELD_TEMPLATE)[1].'.php';
+		if (!file_exists($template)) throw new BW_WebServerError('Secondary object template not found: '.$BW->site->get(Bearweb_Site::FIELD_TEMPLATE)[1], 500);
 		include $template;
 	}
 ?>
